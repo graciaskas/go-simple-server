e62db41cf12c74a7d2874d622e69a617d8b3602e
Gracias Kasongo <makaveligracias@ao.com>
2022-05-18 13:16:17 +0200

    updated

--- main.go
+++ main.go
@@ -5,3 +5,16 @@ import(
 	"log",
 	"net/http"
 )
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.printf("Server running on http://localhost:8000\n")
+
+	if err := http.listenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
